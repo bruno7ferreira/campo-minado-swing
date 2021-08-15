b0a50a05781cf35947eb431b405d5f6bc1805246
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-15 18:52:09 -0400

    Criando TelaPrincipal.java

---
+++ src/br/com/bruno/campo_minado/visao/TelaPrincipal.java
@@ -0,0 +1,30 @@
+package br.com.bruno.campo_minado.visao;
+
+import br.com.bruno.campo_minado.modelo.Tabuleiro;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("ALL")
+public class TelaPrincipal extends JFrame {
+
+    public TelaPrincipal() {
+        Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+
+
+        setTitle("Campo Minado");//titulo do jogo
+        setSize(690, 438); // tamanho da tela do jogo
+        setLocationRelativeTo(null); // tela centralizada ao monitor
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);// para finalizar o programa quando a janela for fechada
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        new TelaPrincipal();
+
+
+    }
+
+
+}
